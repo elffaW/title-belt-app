23628a6c50c3b760492665b46b02a5d79c20fed6
mike <mike>
2024-09-30 19:34:19 -0400

    basic api endpoints

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from title_belt_nhl.schedule import Schedule
+
+from .models import NhlTeams
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Try /belt_holder or /teams"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/belt_holder")
+async def get_belt_holder():
+    schedule = Schedule(team, season)
+    holder = schedule.belt_holder
+
+    return {"holder": holder, "season": schedule.season}
+
+@app.get("/teams/{team_id}")
+async def get_belt_opportunity(team_id: NhlTeams):
+    if team_id not in NhlTeams:
+        raise HTTPException(status_code=404, detail=f"Team {team_id} not found")
+
+    schedule = Schedule(team, season)
+    holder = schedule.belt_holder
+
+    path = schedule.find_nearest_path([holder], holder)
+    games = path.split("vs")
+
+    return {
+        "holder": holder,
+        "numGames": len(games) - 1,
+        "team": team,
+        "path": path,
+    }
+
+@app.get("/heartbeat")
+def heartbeat():
+    return {"message": "Success!"}
---
+++ models.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+class NhlTeams(str, Enum):
+    NJD
+    BUF
+    STL
+    BOS
+    CHI
+    TOR
+    NYR
+    WPG
+    CGY
+    COL
+    MTL
+    LAK
+    FLA
+    PIT
+    UTA
+    DAL
+    CBJ
+    TBL
+    PHI
+    OTT
+    NSH
+    CAR
+    NYI
+    SEA
+    ANA
+    MIN
+    DET
+    VAN
+    VGK
+    SJS
+    EDM
+    WSH
