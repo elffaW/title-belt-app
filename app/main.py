c95c05f157215aa6ea4d3dcaa4658bb5631f8f26
elffaW <elffaW@users.noreply.github.com>
2024-10-08 19:30:19 -0400

    feat: show all games in hierarchy with shortest path highlighted (#3)
    Co-authored-by: mike <mike>

--- app/main.py
+++ app/main.py
@@ -15,9 +15,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 def _build_full_page_context(request: Request):
     teams = [t for t in NhlTeams]
-    selected_team = ""
     schedule = Schedule("")
     holder = schedule.belt_holder
+    selected_team = holder
 
     return {
         "request": request,
@@ -26,6 +26,7 @@ def _build_full_page_context(request: Request):
         "teams": teams,
         "team": selected_team,
         "season": schedule.get_season_pretty(),
+        "schedule": [],
     }
 
 
